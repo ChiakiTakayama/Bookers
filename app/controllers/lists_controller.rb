48d2cf4010a0872315a5d82d822bdf88a2996a51
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-24 08:05:37 +0000

    [update]ストロングパラメータ入力まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,11 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+  def cleate
+    list = List.new(list_params)
+    list.save
+    redirect_to'/top'
   end
 
   def index
@@ -9,5 +15,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def list_params
+    oarams.require(:list).permit(:title,:body)
   end
 end
